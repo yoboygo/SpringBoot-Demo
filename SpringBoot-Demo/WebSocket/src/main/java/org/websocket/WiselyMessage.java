048c965e5a663a5ef974732eb2ed4b93b83aa872
yoboygo <yoboygo@gmail.com>
2018-01-07 17:02:43 +0800

    初始化

---
+++ SpringBoot-Demo/WebSocket/src/main/java/org/websocket/WiselyMessage.java
@@ -0,0 +1,20 @@
+package org.websocket;
+
+/**
+ * 浏览器想服务器发送的消息
+ * @author bpqqo
+ *
+ */
+public class WiselyMessage {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
